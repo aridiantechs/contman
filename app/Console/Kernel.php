d42e014ffe16ea38a447ff0c14bf8e7b7fbee85d
jawad-gem <jawadahmed1024@gmail.com>
2023-02-09 10:53:59 +0500

    cron setup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $contracts=\App\Models\Contract::whereDate('end_date','=',\Carbon\Carbon::now()->format('Y-m-d'))->whereNull('status')->orderBy('end_date','asc')->get();
         // $schedule->command('inspire')->hourly();
         $schedule->job(new ContractRenewJob)->everyMinute()->withoutOverlapping();
         $schedule->job(new ContractEndReminderJob)->everyMinute()->withoutOverlapping();
