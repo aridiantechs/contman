fd953e36374659969558dd526c4f0ca720b062e5
jawad-gem <jawadahmed1024@gmail.com>
2023-02-11 18:32:15 +0500

    search

--- resources/views/backend/user/list.blade.php
+++ resources/views/backend/user/list.blade.php
@@ -98,7 +98,7 @@
         <div class="card">
             <div class="card-body">
                 <form class="form-row" action="{{ getFullUrl() }}">
-                    <h3 class="col-md-12">Search contracts</h3>
+                    <h3 class="col-md-12">Search</h3>
                     <hr>
                     <div class="form-group col-md-4">
                         <input type="text" class="form-control" id="search_text" name="search_text" value="{{request()->query('search_text')}}">
