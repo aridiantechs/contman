2d70768aaccb0d46e36ca4b8e23c9ef1e7614701
jawad-gem <jawadahmed1024@gmail.com>
2023-02-11 19:34:19 +0500

    danish

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -25,7 +25,7 @@
                                     <p class="m-b-0 text-muted">Revenue</p>
                                     <div class="d-flex justify-content-between align-items-center">
                                         <i class="fas fa-money-bill stats-icon"></i>
-                                        <h2 class="m-b-0 stats-val">{{$data['revenue'] ?? ''}}</h2>
+                                        <h2 class="m-b-0 stats-val">{{danishFormat($data['revenue'] ?? '')}}</h2>
                                     </div>
                                     {{-- <span class="badge badge-pill badge-cyan font-size-12">
                                         <i class="anticon anticon-arrow-up"></i>
